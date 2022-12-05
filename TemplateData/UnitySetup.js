6b2fabfb7a41c404a5699f4322df11450a193626
Helmaay <mohamed.helmy@5d-VR.com>
2022-12-05 18:57:11 +0200

    .

---
+++ TemplateData/UnitySetup.js
@@ -0,0 +1,53 @@
+
+// Shows a temporary message banner/ribbon for a few seconds, or
+// a permanent error message on top of the canvas if type=='error'.
+// If type=='warning', a yellow highlight color is used.
+// Modify or remove this function to customize the visually presented
+// way that non-critical warnings and error messages are presented to the
+// user.
+
+
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/Test.loader.js";
+var config = {
+    dataUrl: buildUrl + "/Test.data",
+    frameworkUrl: buildUrl + "/Test.framework.js",
+    codeUrl: buildUrl + "/Test.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "PhotonReadyPlayerMeVuplex",
+    productVersion: "0.1",
+};
+
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
+
+if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    container.className = "unity-mobile";
+    // Avoid draining fillrate performance on mobile devices,
+    // and default/override low DPI mode on mobile browsers.
+    config.devicePixelRatio = window.devicePixelRatio;
+}
+
+
+loadingBar.style.display = "block";
+
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+        progressBarFull.style.width = 100 * progress + "%";
+    })
+        .then((unityInstance) => {
+            unityGame = unityInstance;
+            loadingBar.style.display = "none";
+        })
+        .catch((message) => {
+            alert(message);
+        });
+};
+document.body.appendChild(script);
